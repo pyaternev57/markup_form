4463f3fe2819ae24f96cd29c50db2fb5d66e1cb9
Ivan <pyaternev@gmail.com>
2021-02-07 18:11:42 +0300

    get_notebook_id is working now

--- flask_login_tutorial/models.py
+++ flask_login_tutorial/models.py
@@ -6,133 +6,139 @@ from . import db
 
 
 class User(UserMixin, db.Model):
-	"""User account model."""
+    """User account model."""
 
-	__tablename__ = 'users'
-	id = db.Column(
-		db.Integer,
-		primary_key=True
-	)
-	username = db.Column(
-		db.String(100),
-		nullable=False,
-		unique=False
-	)
-	password = db.Column(
-		db.String(200),
-		primary_key=False,
-		unique=False,
-		nullable=False
-	)
-	created_on = db.Column(
+    __tablename__ = 'users'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    username = db.Column(
+        db.String(100),
+        nullable=False,
+        unique=False
+    )
+    password = db.Column(
+        db.String(200),
+        primary_key=False,
+        unique=False,
+        nullable=False
+    )
+    created_on = db.Column(
         db.DateTime,
         index=False,
         unique=False,
         nullable=True
     )
 
-	def set_password(self, password):
-		"""Create hashed password."""
-		self.password = generate_password_hash(password, method='sha256')
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(password, method='sha256')
 
-	def check_password(self, password):
-		"""Check hashed password."""
-		return check_password_hash(self.password, password)
+    def check_password(self, password):
+        """Check hashed password."""
+        return check_password_hash(self.password, password)
 
-	def __repr__(self):
-		return '<User {}>'.format(self.username)
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
 
 class Notebook(db.Model):
-	"""table with notebook data"""
+    """table with notebook data"""
 
-	__tablename__ = 'notebooks'
-	id = db.Column(
-		db.Integer,
-		primary_key=True
-	)
-	link = db.Column(
-		db.String(100),
-		nullable=False,
-		unique=False
-	)
-	created_on = db.Column(
+    __tablename__ = 'notebooks'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    link = db.Column(
+        db.String(100),
+        nullable=False,
+        unique=False
+    )
+    created_on = db.Column(
         db.DateTime,
         index=False,
         unique=False,
         nullable=True
     )
+
 
 class Chunk(db.Model):
-	"""table with chunks data"""
+    """table with chunks data"""
 
-	__tablename__ = 'chunks'
-	id = db.Column(
-		db.Integer,
-		primary_key=True
-	)
-	notebook_id = db.Column(
-		db.Integer,
-		nullable=False,
-		unique=False
-	)
-	chunk_id = db.Column(
-		db.Integer,
-		nullable=False,
-		unique=False
-	)
-	data_format = db.Column(
-		db.String(100),
-		nullable=False,
-		unique=False
-	)
-	graph_vertex = db.Column(
-		db.String(100),
-		nullable=False,
-		unique=False
-	)
-	graph_vertex_subclass = db.Column(
-		db.String(100),
-		nullable=False,
-		unique=False
-	)
-	created_on = db.Column(
+    __tablename__ = 'chunks'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    notebook_id = db.Column(
+        db.Integer,
+        nullable=False,
+        unique=False
+    )
+    chunk_id = db.Column(
+        db.Integer,
+        nullable=False,
+        unique=False
+    )
+    data_format = db.Column(
+        db.String(100),
+        nullable=False,
+        unique=False
+    )
+    graph_vertex = db.Column(
+        db.String(100),
+        nullable=False,
+        unique=False
+    )
+    graph_vertex_subclass = db.Column(
+        db.String(100),
+        nullable=False,
+        unique=False
+    )
+    created_on = db.Column(
         db.DateTime,
         index=False,
         unique=False,
         nullable=True
     )
+
 
 class History(db.Model):
-	__tablename__ = 'history'
-	id = db.Column(
-		db.Integer,
-		primary_key=True
-	)
-	notebook_id = db.Column(
-		db.Integer,
-		nullable=False,
-		unique=False
-	)
-	chunk_id = db.Column(
-		db.Integer,
-		nullable=False,
-		unique=False
-	)
-	user = db.Column(
-		db.String(100),
-		nullable=False,
-		unique=False
-	)
-	created_on = db.Column(
+    __tablename__ = 'history'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    notebook_id = db.Column(
+        db.Integer,
+        nullable=False,
+        unique=False
+    )
+    chunk_id = db.Column(
+        db.Integer,
+        nullable=False,
+        unique=False
+    )
+    user = db.Column(
+        db.String(100),
+        nullable=False,
+        unique=False
+    )
+    created_on = db.Column(
         db.DateTime,
         index=False,
         unique=False,
         nullable=True
     )
 
-class Chunk_data:
-	def __init__(self):
-			self.notebook_id = 1
-			self.chunk_id = 1
-			self.href = ""
-			self.data = ""
+
+class ChunkData:
+    notebook_id = 1
+    chunk_id = 1
+    href = ""
+    data = ""
+
+    def __repr__(self):
+        return f"notebook_id: {self.notebook_id}; chunk_id: {self.chunk_id}"
--- flask_login_tutorial/routes.py
+++ flask_login_tutorial/routes.py
@@ -2,10 +2,9 @@
 from flask import Blueprint, redirect, render_template, url_for
 from flask_login import current_user, login_required, logout_user
 from .forms import DataForm
-from .models import Chunk_data, Notebook, Chunk, History, db
+from .models import ChunkData, Notebook, Chunk, History, db
 from sqlalchemy.sql.expression import func
 from .utils import open_pkl
-
 
 # Blueprint Configuration
 main_bp = Blueprint(
@@ -14,19 +13,19 @@ main_bp = Blueprint(
     static_folder='static'
 )
 
+
 def get_notebook_id(db, current_user):
-    print(current_user)
-    notebook_id = db.session.query(func.max(History.notebook_id)).filter_by(user=current_user).first()
-    print(notebook_id)
+    notebook_id = db.session.query(func.max(History.notebook_id)).filter_by(user=current_user.username).first()[0]
     if not notebook_id:
-        notebook_id = History.query.select("max(notebook_id)").first()
         notebook_id = db.session.query(func.max(History.notebook_id)).first()[0]
         if not notebook_id:
             notebook_id = 0
     return notebook_id + 1
 
+
 def get_chunk_id(db, current_user, notebook_id):
-    chunk_id = db.session.query(func.max(Chunk.chunk_id)).filter_by(user=current_user, notebook_id=notebook_id).first()[0]
+    chunk_id = db.session.query(func.max(Chunk.chunk_id)).filter_by(user=current_user, notebook_id=notebook_id).first()[
+        0]
     print(chunk_id)
     if not chunk_id:
         chunk_id = -1
@@ -36,13 +35,15 @@ def get_chunk_id(db, current_user, notebook_id):
         data = get_chunks_from_notebook(conn, link, notebook_id)
         save_pkl(data, f"data/{notebook_id}.pkl")
 
+
 @main_bp.route('/', methods=['GET'])
 @login_required
 def dashboard():
     """Logged-in User Dashboard."""
-    chunk = Chunk_data()
+    chunk = ChunkData()
     form = DataForm()
     chunk.notebook_id = get_notebook_id(db, current_user)
+    print(chunk.notebook_id)
 
     return render_template(
         'dashboard.jinja2',
